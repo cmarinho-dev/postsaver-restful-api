35ed28372eb2a7b719dad55c4fdf21370cfd2c02
cmarinho-dev <cmarinho.dev@gmail.com>
2026-07-08 02:07:41 -0300

    fix(mobile): wrap test widget in ProviderScope

--- frontend/postsaver/test/widget_test.dart
+++ frontend/postsaver/test/widget_test.dart
@@ -1,10 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:postsaver/main.dart';
 
 void main() {
   testWidgets('App builds without errors', (WidgetTester tester) async {
-    await tester.pumpWidget(const AppInit());
+    await tester.pumpWidget(
+      const ProviderScope(child: AppInit()),
+    );
     await tester.pumpAndSettle();
   });
 }
